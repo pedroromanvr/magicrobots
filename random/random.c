d7f29e0bd67365a1f0126ae4bb2306376648011f
unknown <marceloleon123@gmail.com>
2014-01-29 22:40:54 -0600

    Added random module

---
+++ random/random.c
@@ -0,0 +1,11 @@
+
+#include "random.h"
+
+uint16_t randomIdx = 0;
+
+uint16_t random16()
+{
+    uint16_t ret = random8();
+    ret = ((uint16_t)random8())<<8;
+    return ret;
+}
---
+++ random/random.h
@@ -0,0 +1,19 @@
+#ifndef __RANDOM__
+#define __RANDOM__
+
+#include <stdint.h>
+#include "../platform.h"
+
+/*
+ * You should place a random sequence in the eeprom before hand.
+ * CodeVisionAVR will use the required functions to read from the EEPROM
+ */
+eeprom uint8_t numbers[1024]; 
+extern uint16_t randomIdx;
+
+#define random8() \
+    numbers[randomIdx++]
+
+uint16_t random16();
+
+#endif 
